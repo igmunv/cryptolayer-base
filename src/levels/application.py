255d4a9c8050e01d583000d06925d5f898754de6
igmunv <devskynger@gmail.com>
2026-07-13 03:05:25 +0500

    fix: after sign send used send_without_encrypt, now use default send func

--- src/levels/application.py
+++ src/levels/application.py
@@ -21,12 +21,12 @@ class Application(Base):
 
     def send_my_sign(self, sign: bytes):
         packet = ApplicationPacket(PackTypes.SERVICE.value, CMDTypes.MY_SIGN.value, sign)
-        self.LOWER_LEVEL.send_without_encrypt(packet.to_bytes())
+        self.send(packet.to_bytes())
 
 
     def send_my_public_key(self, public_key: bytes):
         packet = ApplicationPacket(PackTypes.SERVICE.value, CMDTypes.MY_PUBLIC_KEY.value, public_key)
-        self.send(packet.to_bytes())
+        self.LOWER_LEVEL.send_without_encrypt(packet.to_bytes())
 
 
     def send_disconnect(self):
